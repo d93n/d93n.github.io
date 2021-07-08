f79ef8b182a34d4687f42417b46cd270bb3ccaba
d93n <86151639+d93n@users.noreply.github.com>
2021-07-08 18:46:37 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $("button").click(function(){
-    alert("Tack för din beställning! Jag kommer att skicka en beställningsbekräftelse till den angivna e-post-adressen när din beställning lästs av mig. Du kan nu stänga denna dialogruta."));
+    alert("Tack för din beställning! Jag kommer att skicka en beställningsbekräftelse till den angivna e-post-adressen när din beställning lästs av mig. Du kan nu stänga denna dialogruta.");
     });
 });
