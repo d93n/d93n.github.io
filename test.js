66cb05b04f341dfbff2482087a05059de78df82c
d93n <86151639+d93n@users.noreply.github.com>
2021-07-08 17:43:00 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $("button").click(function(){
-    alert(document.getElementById("checkout-cart").innerHTML);
+    alert(document.getElementById("Tack för din beställning! Du kan nu stänga denna dialogruta.").innerHTML);
     });
 });
